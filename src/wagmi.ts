d2d1f649612b190869bbd795f47c10de83b183c0
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2023-04-11 22:01:12 -0400

    first commit

---
+++ src/wagmi.ts
@@ -0,0 +1,23 @@
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { configureChains, createClient, } from 'wagmi'
+import { songbird, flare } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [flare, ...(process.env.NODE_ENV === 'development' ? [songbird] : [songbird])],
+  [publicProvider()],
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'TheCooties',
+  chains,
+})
+
+export const client = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+})
+
+export { chains }
